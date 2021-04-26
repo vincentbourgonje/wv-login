c1de00d936ca1fae2757db4f161327d62d073851
Webvise <vincent@webvise.nl>
2021-04-26 18:58:31 +0200

    Added variable prefix to process.php and added some basic errormessages

--- process.php
+++ process.php
@@ -8,12 +8,11 @@
     $setting_redirect_url = get_option('wvlogin-settings-redirect-to');
     $now_date = date_i18n("Y-m-d H:i:s.u");
 
-    $sql = "SELECT user_id FROM wp_usermeta WHERE meta_value='$token_id' AND meta_key='wvlogintoken'";
+    $tablename = $wpdb->prefix . 'usermeta';
+    $sql = "SELECT user_id FROM $tablename WHERE meta_value='$token_id' AND meta_key='wvlogintoken'";
     $user_id = $wpdb->get_var($sql);
 
     $token_valid_until = get_user_meta($user_id, 'wvlogindate');
-
-
 
     if ( !is_user_logged_in() && $now_date <= $token_valid_until ) {
         clean_user_cache($user_id);
@@ -26,4 +25,10 @@
         update_user_meta($user_id, 'wvlogindate', '');
         update_user_meta($user_id, 'wvlogintoken', '');
         wp_redirect($setting_redirect_url);
+    } else {
+        echo '<p>' . __('Invalid login link!','wv-login') . '</p>';
+        if ($now_date > $token_valid_until) {
+            echo '<p>' . __('Token has expired!','wv-login') . '</p>';
+        }
+
     }
